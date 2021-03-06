0d0f8b499777c7b9ffdd9cd7465f7d3adaca7020
Carolina Mamani <mamaniccarolina@gmail.com>
2021-03-06 11:32:12 -0300

    feat: routes added

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.set('PORT', process.env.PORT || 4000);
+
+/*Middlewares*/
+app.use(express.json());
+
+/*Routes*/
+app.use('/productos', require('./routes/productos'));
+app.use('/carritos', require('./routes/carritos'));
+
+/*Starting the server*/
+app.listen(app.get('PORT'), () => {
+  console.log(`Server on port ${app.get('PORT')}`);
+});
---
+++ src/routes/carritos.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:id', (req, res) => {});
+
+router.patch('/:id', (req, res) => {});
+
+router.patch('/:id', (req, res) => {});
+
+module.exports = router;
