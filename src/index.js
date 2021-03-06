70571ceceec162f8d85773a510970a1ee0b3603a
Carolina Mamani <mamaniccarolina@gmail.com>
2021-03-06 14:30:07 -0300

    feat: Producto and Carrito classes added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const administrador = true;
+
 app.set('PORT', process.env.PORT || 4000);
 
 /*Middlewares*/
---
+++ src/services/carritos.service.js
@@ -0,0 +1,7 @@
+class Carrito {
+  constructor(id, timestamp, productos) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.productos = productos;
+  }
+}
