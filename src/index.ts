7a1ec924e43996a579b9dd5a3f8d1cc20be7a8d4
Carolina Mamani <mamaniccarolina@gmail.com>
2021-03-14 20:25:33 -0300

    feat: admin permissions added

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { productsRouter, cartsRouter } from './routes';
 
 const app: Application = express();
 
-const administrador = true;
-
 app.set('PORT', process.env.PORT || 4000);
 
 app.use(express.json());
