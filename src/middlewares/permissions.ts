0a59dba962efdb9579e754dead699c6a16452eb1
Carolina Mamani <mamaniccarolina@gmail.com>
2021-03-14 21:51:58 -0300

    feat: admin permissions added

---
+++ src/middlewares/permissions.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+const admin: boolean = true;
+
+const permissions = {
+  isAdmin: (req: Request, res: Response, next: NextFunction) => {
+    if (admin) {
+      return next();
+    } else {
+      return res.status(401).send({ error: '401 Unauthorized' });
+    }
+  },
+};
+
+export default permissions;
